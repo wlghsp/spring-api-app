3023d579aab8420009b3d84ecf687766e02ab805
CHOIJIHO <wlghsp@gmail.com>
2023-05-14 12:12:43 +0900

    카카오 토큰 발급 구현 (2) - 토큰 받기

--- src/main/java/com/app/web/kakaotoken/controller/KakaoTokenController.java
+++ src/main/java/com/app/web/kakaotoken/controller/KakaoTokenController.java
@@ -1,6 +1,9 @@
 package com.app.web.kakaotoken.controller;
 
+import com.app.web.kakaotoken.client.KakaoTokenClient;
+import com.app.web.kakaotoken.dto.KakaoTokenDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,6 +12,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 public class KakaoTokenController {
 
+    private final KakaoTokenClient kakaoTokenClient;
+
+    @Value("${kakao.client.id}")
+    private String clientId;
+
+    @Value("${kakao.client.secret}")
+    private String clientSecret;
 
     @GetMapping("/login")
     public String login() {
@@ -17,7 +27,17 @@ public class KakaoTokenController {
 
     @GetMapping("/oauth/kakao/callback")
     public @ResponseBody String loginCallback(String code) {
-        return code;
+        String contentType = "application/x-www-form-urlencoded;charset=utf-8";
+        KakaoTokenDto.Request kakaoTokenRequestDto = KakaoTokenDto.Request.builder()
+                .client_id(clientId)
+                .client_secret(clientSecret)
+                .grant_type("authorization_code")
+                .code(code)
+                .redirect_uri("http://localhost:8080/oauth/kakao/callback")
+                .build();
+        KakaoTokenDto.Response kakaoToken = kakaoTokenClient.requestKakaoToken(contentType, kakaoTokenRequestDto);
+
+        return "kakao token : " + kakaoToken.toString();
     }
 
 }
