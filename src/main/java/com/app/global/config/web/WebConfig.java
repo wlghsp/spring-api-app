711717469ac9984b23cc8386495da72eed91fa9f
JIHO CHOI <wlghsp@gmail.com>
2024-09-18 13:43:15 +0900

    인증 인터셉터 구현 (1)

--- src/main/java/com/app/global/config/web/WebConfig.java
+++ src/main/java/com/app/global/config/web/WebConfig.java
@@ -1,12 +1,18 @@
 package com.app.global.config.web;
 
+import com.app.global.interceptor.AuthenticationInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthenticationInterceptor authenticationInterceptor;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -21,5 +27,16 @@ public class WebConfig implements WebMvcConfigurer {
                         HttpMethod.OPTIONS.name()
                 )
                 .maxAge(3600);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationInterceptor)
+                .order(1)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/api/oauth/login",
+                        "/api/access-token/issue",
+                        "/api/logout",
+                        "/api/health");
     }
 }
